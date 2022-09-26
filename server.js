684d79e49bdef3a987a703bab2aadbb13db3b1ae
daniilv-me <me@daniilv.me>
2022-09-25 23:29:01 -0400

    Completed Controller, Routers and Views using express

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
-const connect = require('connect');
-const app = connect();
+// Third-Party Modules
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import session from 'express-session';
 
-function logger(req, res, next){
-    console.log(req.method, req.url);
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-    next();
-}
+//ES Modules fix for __dirname
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-function helloWorld(req, res, next) {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Test World</h1>');
-};
 
-function goodbyeWorld(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('GoodBye World');
-};
+// Import Routes
+import indexRouter from './app/routes/index.route.server.js'
 
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
+// Instantiate Express Application
+const app = express();
+
+//Set up middlewares
+
+//Setup ViewEngine EJS
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/client')));
+app.use(session({
+    secret: 'MySecret',
+    saveUninitialized: false,
+    resave: false
+}))
+
+
+//Use Routes
+app.use('/', indexRouter);
 
 app.listen(3000);
 
